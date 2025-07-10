216fe03361ece46a33fb9520b8e3f35d8f6d7227
Reece Taylor <reecesamueltaylor@gmail.com>
2025-07-10 14:13:18 +0100

    Update lcd.h

--- include/lcd.h
+++ include/lcd.h
@@ -2,11 +2,11 @@
 #ifndef LCD_H
 #define LCD_H
 #include <avr/io.h>
-void lcd_initialise();          // Fixed the typo here
-void updateLCD(int xValue, int yValue, int potValue);  // Added parameters
-void lcdcmdwrite(uint8_t data);  // Added parameter
-void lcdwrite(const char *data_l); // Added parameter
-void sendNibble(uint8_t data);  // Added parameter
-char* intToString(int num);    // Added parameter
+void lcd_initialise();       
+void updateLCD(int xValue, int yValue, int potValue);  
+void lcdcmdwrite(uint8_t data);  
+void lcdwrite(const char *data_l);
+void sendNibble(uint8_t data);  
+char* intToString(int num);  
 
 #endif
